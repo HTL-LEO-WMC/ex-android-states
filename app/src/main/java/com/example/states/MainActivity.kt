630b064135e99b3db1b202257313c0e5ab34fe0e
Gerald <gerald@unterrainer.info>
2026-03-19 10:52:11 +0100

    add viewModel refactoring

--- app/src/main/java/com/example/states/MainActivity.kt
+++ app/src/main/java/com/example/states/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import android.util.Log
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.states.ui.theme.StatesTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,16 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             StatesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    //Variable(
-                    //    modifier = Modifier.padding(innerPadding)
-                    //)
-                    //StateMutable(
-                    //    modifier = Modifier.padding(innerPadding)
-                    //)
-                    //StateRemember(
-                    //    modifier = Modifier.padding(innerPadding)
-                    //)
-                    StateRememberSaveable(
+                    CounterScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -45,85 +37,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-/**
- * Most intuitive solution.
- * - Build - doesn't work
- * - Add logging-line > look in logcat > works
- * - It's not a compose-state, so it doesn't trigger a recomposition
- */
 @Composable
-fun Variable(modifier: Modifier = Modifier) {
-    Log.d("ComposeTest", ">>> Recomposition")
-    var counter = 0;
+fun CounterScreen(
+    modifier: Modifier = Modifier,
+    counterViewModel: CounterViewModel = viewModel()
+) {
     Column(modifier.fillMaxSize()) {
-        Text("counter is $counter")
-        Button(onClick = {
-            counter++
-            Log.d("ComposeTest", "counter = $counter")
-        }) {
-            Text("counter++")
-        }
-    }
-}
-
-/**
- * MutableState is a compose-state.
- * - Build - doesn't work
- * - Add logging-line > look in logcat > works
- * - It's a compose-state, so it triggers a recomposition
- * - But during recomposition the old value is lost (not remembered)
- */
-@SuppressLint("UnrememberedMutableState")
-@Composable
-fun StateMutable(modifier: Modifier = Modifier) {
-    Log.d("ComposeTest", ">>> Recomposition")
-    val counter = mutableStateOf(0)
-    Column(modifier.fillMaxSize()) {
-        Text("counter is ${counter.value}")
-        Button(onClick = {
-            counter.value++
-            Log.d("ComposeTest", "counter = $counter")
-        }) {
-            Text("counter++")
-        }
-    }
-}
-
-/**
- * We remember the state now.
- * - Works
- * - But on screen-rotation the state is still lost, since the fragment is re-created
- */
-@Composable
-fun StateRemember(modifier: Modifier = Modifier) {
-    Log.d("ComposeTest", ">>> Recomposition")
-    val counter = remember { mutableStateOf(0) } // try a screen-rotation now.
-    Column(modifier.fillMaxSize()) {
-            Text("counter is ${counter.value}")
-            Button(onClick = {
-                counter.value++
-                Log.d("ComposeTest", "counter = $counter")
-            }) {
-            Text("counter++")
-        }
-    }
-}
-
-/**
- * We remember the state now.
- * - Works
- * - And on screen-rotation the state is remembered
- */
-@Composable
-fun StateRememberSaveable(modifier: Modifier = Modifier) {
-    Log.d("ComposeTest", ">>> Recomposition")
-    val counter = rememberSaveable { mutableStateOf(0) } // try a screen-rotation now.
-    Column(modifier.fillMaxSize()) {
-        Text("counter is ${counter.value}")
-        Button(onClick = {
-            counter.value++
-            Log.d("ComposeTest", "counter = $counter")
-        }) {
+        Text("counter is ${counterViewModel.counter}")
+        Button(onClick = { counterViewModel.increment() }) {
             Text("counter++")
         }
     }
